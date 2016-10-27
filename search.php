9b524da2858c8d45e8863c5bc8f2bf0e70900967
diamant1991 <diamant1988@mail.ru>
2016-10-27 10:31:58 +0300

    add files

---
+++ search.php
@@ -0,0 +1,220 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Search Results</title>
+    <meta charset="utf-8">
+    <meta name="format-detection" content="telephone=no"/>
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="css/grid.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/search.css"/>
+
+    <script src="js/jquery.js"></script>
+    <script src="js/jquery-migrate-1.2.1.js"></script>
+
+    <!--[if lt IE 9]>
+    <html class="lt-ie9">
+    <div style=' clear: both; text-align:center; position: relative;'>
+        <a href="http://windows.microsoft.com/en-US/internet-explorer/..">
+            <img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820"
+                 alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."/>
+        </a>
+    </div>
+    <script src="js/html5shiv.js"></script>
+  	<script src='js/selectivizr-min.js'></script>
+    <![endif]-->
+
+    <script src='js/device.min.js'></script>
+</head>
+
+<body>
+<div class="page">
+    <!--========================================================
+                              HEADER
+    =========================================================-->
+     <header class="header">
+    <div class="header__top bg-primary">
+      <div class="container">
+          <div class="row">
+              <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                <div class="inline-box-1">
+                  <div class="inline-box-1__content">
+                      <div class="img_container">
+                          <div class="img">
+                            <img src="images/icon-1.png" alt="img">
+                          </div>
+                      </div>
+                  </div>
+                  <div class="inline-box-1__content">
+                    <p>Принимаем заказы круглосуточно</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                 <div class="inline-box-1">
+                  <div class="inline-box-1__content">
+                      <div class="img_container">
+                           <div class="img">
+                            <img src="images/icon-2.png" alt="img">
+                          </div>
+                      </div>
+                  </div>
+                  <div class="inline-box-1__content">
+                    <p>Доставка по всей России</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                 <div class="inline-box-1">
+                  <div class="inline-box-1__content">
+                      <div class="img_container">
+                           <div class="img">
+                              <img src="images/icon-3.png" alt="img">
+                          </div>
+                      </div>
+                  </div>
+                  <div class="inline-box-1__content">
+                    <p>Оплата при получении</p>
+                  </div>
+                </div>
+              </div>
+              <div class="col-xs-12 col-sm-6 col-md-3 col-lg-3">
+                  <div class="inline-box-1">
+                    <div class="inline-box-1__content">
+                        <div class="img_container">
+                             <div class="img">
+                            <img src="images/icon-4.png" alt="img">
+                          </div>
+                        </div>
+                    </div>
+                    <div class="inline-box-1__content">
+                      <p>Гарантия 3 года</p>
+                    </div>
+                </div>
+                </div>
+            </div>
+          </div>
+      </div>
+      <div class="header__bottom">
+          <div class="container">
+            <div class="row">
+              <div class="col-xs-12 col-sm-3 col-md-3 col-lg-3">
+                  <div class="brand top-brand">
+                    <h1 class="brand__name">
+                      <a class="brand__name--link"  href="#">
+                        <img src="images/logo.png" alt="img">
+                      </a>
+                    </h1>
+                  </div>
+              </div>
+              <div class="col-xs-5 col-sm-3 col-md-3 col-lg-3">
+                <p class="text-1">Интернет-магазин модульных картин,постеров и репродукций.Фотопечать и роспись маслом</p>
+              </div>
+              <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                <div class="contacts">
+                  <a href="callto:88007757637" class="contacts__link">8 (800) 775-76-37</a>
+                  <a href="callto:88007757637" class="contacts__link">8 (800) 775-76-37</a>
+                  <p class="contacts__info">Пн - Пт 9:00 - 19:00, Сб, Вс - выходной. Звонок по РФ бесплатный</p>
+                  <a href="callto:" class="contacts__freecall">Заказать бесплатный звонок</a>
+                </div>
+              </div>
+              <div class="col-xs-3 col-sm-2 col-md-2 col-lg-2">
+              <!-- Shoping-cart -->
+                <div class="inline-box-2">
+                  <div class="inline-box-2__content">
+                    <p><a href="#">Корзина</a></p>
+                  </div>
+                    <div class="inline-box-2__content">
+                      <div class="img_conteiner shoping-cart">
+                        <img src="images/shoping-icon.png" alt="">
+                        <span class="shoping-cart__counter">2</span>
+                      </div>
+                    </div>
+                </div>
+              <!-- //Shoping-cart -->
+              </div>
+            </div>
+          </div>
+      </div>
+    <div class="header__main bg-secondary">
+      <div id="stuck_container" class="stuck_container">
+        <div class="container">
+          <div class="row">
+            <nav class="header__nav nav">
+              <ul class="sf-menu" data-type="navbar">
+                <li class="sf-menu__item sf-menu__active">
+                  <a href="index.html" class="sf-menu__item--link sf-menu__active--link">О нас</a>
+                </li>
+                <li class="sf-menu__item">
+                  <a href="index-1.html" class="sf-menu__item--link">Каталог Товаров</a>
+                </li>
+                <li class="sf-menu__item">
+                  <a href="index-2.html" class="sf-menu__item--link">Гарантии</a>
+                </li>
+                <li class="sf-menu__item">
+                  <a href="index-3.html" class="sf-menu__item--link">Доставка и Оплата</a>
+                </li>
+                <li class="sf-menu__item">
+                  <a href="index-4.html" class="sf-menu__item--link">Фото Товара</a>
+                </li>
+                <li class="sf-menu__item">
+                  <a href="index-5.html" class="sf-menu__item--link">Сотрудничество</a>
+                </li>
+                <li class="sf-menu__item">
+                  <a href="index-5.html" class="sf-menu__item--link">Контакты</a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </header>
+
+    <!--========================================================
+                              CONTENT
+    =========================================================-->
+    <section id="content" class="well content">
+        <div class="container ">
+            <div class="row">
+                <h4>Search Results</h4>
+                <div id="search-results"></div>
+            </div>
+        </div>
+    </section>
+
+    <!--========================================================
+                              FOOTER
+    =========================================================-->
+    <footer class="footer bg-primary">
+        <div class="container">
+          <div class="row">
+            <div class="col-xs-12 col-sm-6 col-sm-offset-3 col-md-6 col-md-offset-3  col-lg-6 col-lg-offset-3 ">
+              <ul class="pay-systems">
+                <li class="pay-systems__item">
+                  <a href="#" class="pay-systems__item--link">
+                    <img src="images/pay-icon01.png" height="30" width="102" alt="img">
+                  </a>
+                </li>
+                <li class="pay-systems__item">
+                  <a href="#" class="pay-systems__item--link">
+                    <img src="images/pay-icon02.png" alt="img">
+                  </a>
+                </li>
+                <li class="pay-systems__item">
+                  <a href="#" class="pay-systems__item--link">
+                    <img src="images/pay-icon03.png" alt="img">
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+    </footer>
+</div>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
